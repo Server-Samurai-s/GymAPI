f34305dfa4a93a019bff3d3b96489227a4f4ef3b
st10082679 <st10082679@vcconnect.edu.za>
2024-09-28 15:43:37 +0200

    Add muscle groups

--- src/app.js
+++ src/app.js
@@ -2,22 +2,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+//----------------------------------------------------------------------//
 // Import routes
 const workoutRoutes = require('./routes/workouts');
-
+//----------------------------------------------------------------------//
 // Initialize Express app
 const app = express();
-
+//----------------------------------------------------------------------//
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
+//----------------------------------------------------------------------//
 // API routes
 app.use('/api/workouts', workoutRoutes);
-
+//----------------------------------------------------------------------//
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+//----------------------------------------------------------------------//
